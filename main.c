e7b9514e52406ed90c11233cb4fdf83ee768f944
khoa nguyen <khoa.nguyen>
2021-03-25 19:08:41 +0700

    learn to work with remote repo

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
+march 25 
 
-
-int main () 
-
-
-hello i'm khoa batman  
-
-im khoa 
-in branch master 
-
-
-lal ala la ala 
+i just realize you can use git checkout to create a local branch that matches a existing remote branch with the same name and those branches are linked
+cool right :v 
+so the idea is: 
+	git clone 
+	git pull 
+	git checkout <remote branch> 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+so in git you can't commit empty folder
+to commit those folder, put some dumb files called "placeholder" there and commit :) 
